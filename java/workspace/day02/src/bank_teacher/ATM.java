bae574cd941ef99160ea52d4db60e3525647b261
mingJ7235 <joshuara7235@gmail.com>
2021-01-21 21:50:18 +0900

    Bank Example homework

--- java/workspace/day02/src/bank_teacher/ATM.java
+++ java/workspace/day02/src/bank_teacher/ATM.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ATM {
-	
 	//참고
 	/*
 	 * int choice = 1;
@@ -48,7 +47,7 @@ public class ATM {
 
 		boolean check = false;
 
-		int choice = 0;
+		int bankChoice = 0;
 		int optionChoice = 0;
 		int customerNum = 100;
 		
@@ -64,19 +63,16 @@ public class ATM {
 		}
 		
 		Bank[][] arrBank = {arK, arS, arW};
-
 		//각 은행별 고객 수 
 		int[] arCustomerCnt = {Kookmin.cnt,Shinhan.cnt,Woori.cnt};
-		
 		//ATM 로직 시작
 		while (true) {
 			System.out.println(mainMenu);
-			choice = sc.nextInt();
-			if (choice == 4) {
+			bankChoice = sc.nextInt();
+			if (bankChoice == 4) {
 				System.out.println("ATM을 종료합니다.");
 				break;
 			}
-
 			while (true) {
 				System.out.println(choiceMenu);
 				optionChoice = sc.nextInt();
@@ -85,7 +81,6 @@ public class ATM {
 					System.out.println("뒤로 돌아갑니다.");
 					break; 
 				}
-				
 				switch (optionChoice) {
 				//계좌 개설
 				case 1:
@@ -115,124 +110,134 @@ public class ATM {
 					} while (pw.length() != 4);	
 					
 					//휴대폰 번호
-					while (true) {
+					 do{
 						System.out.print("고객님의 핸드폰 번호를 적어주세요 (-는 제외합니다) : ");
 						phone = sc.next();
 						try {
-							//�޴��� ��ȣ�� ������ ����ȯ �� ������ �߻����� �ʴ´ٸ� ���� �Է�
 							Integer.parseInt(phone);
-							//�����Է½� break �ߵ�
+						} catch (Exception e) {
+							System.out.println("숫자만 입력할 수 있습니다..");
 							break;
-						} catch (NumberFormatException e) {
-							//���� �Է��� �ƴ� �� ���ܰ� �߻��Ͽ� ����
-							System.out.println("���ڸ� �Է��� �� �ֽ��ϴ�.");
 						}
-					}
+						
+						check = false;
+						for (int i = 0; i < arrBank.length; i++) {
+							for (int j = 0; j < arCustomerCnt[i]; j++) {
+								if(arrBank[i][j].phone.equals(phone)) {
+									check = true;
+								}
+							}
+						}
+					}while (check);
 					
-					//�ű� �� �ʵ��� �ּҸ� temp�� ����ش�.
-					//(��)choice - 1 : ����ڰ� ������ ����
-					//(��)arUserCnt[choice - 1] : ����ڰ� ������ ������ ȸ�� ��(6�϶� 6��° �ε����� �ű�ȸ���ڸ�)
-					Bank temp = arrBank[choice - 1][arCustomerCnt[choice - 1]];
+					Bank customer = arrBank[bankChoice - 1][arCustomerCnt[bankChoice - 1]];
 					
-					temp.name = name;
-					temp.account = account;
-					temp.pw = pw;
-					temp.phone = phone;
-					
-					System.out.println("�������մϴ١�");
-					System.out.println(name + "���� ���¹�ȣ : " + account);
-					System.out.println("�������� ���� �̿� �����մϴ�^^");
-					
-					//���� �Ϸ�� �ش� ���� ȸ�� �� 1����
-					arCustomerCnt[choice - 1]++;
+					customer.name = name;
+					customer.account = account;
+					customer.pw = pw;
+					customer.phone = phone;
+					System.out.println(name + "고객님의 "+ account+"계좌가 생성되었습니다.");
+					arCustomerCnt[bankChoice - 1]++;
 					break;
-				//���¹�ȣ�� ��й�ȣ�� �Է��ؾ� ���� �̿��� �����ϴ�.
-				// �Ա��ϱ�
+				
+				//입금
 				case 2:
-					System.out.print("���¹�ȣ : ");
+					System.out.print("입금할 계좌 번호 : ");
 					account = sc.next();
-					
-					System.out.print("��й�ȣ : ");
+					System.out.print("비밀번호 4자리 : ");
 					pw = sc.next();
 					
-					//arUserCnt[choice - 1] : �ش� ������ ȸ����ŭ�� �ݺ�
-					for (int i = 0; i < arCustomerCnt[choice - 1]; i++) {
-						//�α��� �˻�
-						if(arrBank[choice - 1][i].account.equals(account)
-								&& arrBank[choice - 1][i].pw.equals(pw)) {
-							//�α��� ������ check�� true
+					for (int i = 0; i < arCustomerCnt[bankChoice - 1]; i++) {
+						if(arrBank[bankChoice - 1][i].account.equals(account)
+								&& arrBank[bankChoice - 1][i].pw.equals(pw)) {
 							check = true;
-							System.out.print("�Աݾ� : ");
-							arrBank[choice - 1][i].deposit(sc.nextInt());
-							//�Ա� �� ���� �ܾ� ǥ��
-							System.out.println("���� �ܾ� : " + arrBank[choice - 1][i].money + "��");
+							System.out.print("입금하실 금액을 입력해주세요 : ");
+							arrBank[bankChoice - 1][i].deposit(sc.nextInt());
+							System.out.println(arrBank[bankChoice-1][i].name + "고객님의 잔액 : " 
+									+ arrBank[bankChoice - 1][i].money + "원");
 							break;
 						}
-					}
+					}//로그인 실패의 경우
 					if(!check) {
-						System.out.println("���¹�ȣ Ȥ�� ��й�ȣ�� Ȯ�����ּ���");
+						System.out.println("입금 계좌 번호, 비밀번호를 확인해주세요");
 					}
 					break;
-				// ����ϱ�
+					
+				// 출금
 				case 3:
-					System.out.print("���¹�ȣ : ");
+					System.out.print("출금할 계좌 번호 : ");
 					account = sc.next();
 					
-					System.out.print("��й�ȣ : ");
+					System.out.print("비밀번호 4자리 : ");
 					pw = sc.next();
 					
-					//arUserCnt[choice - 1] : �ش� ������ ȸ����ŭ�� �ݺ�
-					for (int i = 0; i < arCustomerCnt[choice - 1]; i++) {
-						//�α��� �˻�
-						if(arrBank[choice - 1][i].account.equals(account)
-								&& arrBank[choice - 1][i].pw.equals(pw)) {
-							//�α��� ������ check�� true
+					for (int i = 0; i < arCustomerCnt[bankChoice - 1]; i++) {
+						if(arrBank[bankChoice - 1][i].account.equals(account)
+								&& arrBank[bankChoice - 1][i].pw.equals(pw)) {
 							check = true;
-							System.out.print("��ݾ� : ");
-							if(arrBank[choice - 1][i].withdraw(sc.nextInt())) {
-								//��� �� ���� �ܾ� ǥ��
-								System.out.println("���� �ܾ� : " + arrBank[choice - 1][i].money + "��");
+							System.out.print("출금할 금액을 입력해주세요 : ");
+							if(arrBank[bankChoice - 1][i].withdraw(sc.nextInt())) {
+								System.out.println(arrBank[bankChoice-1][i].name +"고객님의 잔액 : " 
+										+ arrBank[bankChoice - 1][i].money + "원");
 							}else {
-								System.out.println("��� ���� / �ܾ� ����");
+								System.out.println("잔액부족으로 출금하지 못했습니다.");
 							}
 							break;
 						}
-					}
+					}//로그인 실패
 					if(!check) {
-						System.out.println("���¹�ȣ Ȥ�� ��й�ȣ�� Ȯ�����ּ���");
+						System.out.println("입금 계좌 번호, 비밀번호를 확인해주세요");
 					}
 					break;
-				// �ܾ���ȸ
+				// 잔액조회
 				case 4:
-					System.out.print("���¹�ȣ : ");
+					System.out.print("조회할 계좌번호 : ");
 					account = sc.next();
 					
-					System.out.print("��й�ȣ : ");
+					System.out.print("비밀번호 4자리: ");
 					pw = sc.next();
 					
-					//arUserCnt[choice - 1] : �ش� ������ ȸ����ŭ�� �ݺ�
-					for (int i = 0; i < arCustomerCnt[choice - 1]; i++) {
-						//�α��� �˻�
-						if(arrBank[choice - 1][i].account.equals(account)
-								&& arrBank[choice - 1][i].pw.equals(pw)) {
-							//�α��� ������ check�� true
+					for (int i = 0; i < arCustomerCnt[bankChoice - 1]; i++) {
+						if(arrBank[bankChoice - 1][i].account.equals(account)
+								&& arrBank[bankChoice - 1][i].pw.equals(pw)) {
 							check = true;
-							System.out.println("������ : " + arrBank[choice - 1][i].name);
-							System.out.println("���¹�ȣ : " + arrBank[choice - 1][i].account);
-							System.out.println("���� �ܾ� : " + arrBank[choice - 1][i].getBalance() + "��");
+							System.out.println("고객명 : " + arrBank[bankChoice - 1][i].name +
+									"계좌번호 : " + arrBank[bankChoice - 1][i].account+
+									"현재" +arrBank[bankChoice - 1][i].name+"님의 계좌 잔액 : " 
+									+ arrBank[bankChoice - 1][i].getBalance() + "원");
 							break;
 						}
 					}
 					if(!check) {
-						System.out.println("���¹�ȣ Ȥ�� ��й�ȣ�� Ȯ�����ּ���");
+						System.out.println("입금 계좌 번호, 비밀번호를 확인해주세요");
 					}
 					break;
+	            case 5: //계좌번호 찾기
+	                System.out.print("고객님 핸드폰 번호: ");
+	                phone = sc.next();
+	                
+	                System.out.print("비밀번호 4자리: ");
+	                pw = sc.next();
+	                
+	                Bank temp = null;
+	                for (int i = 0; i < arrBank.length; i++) {
+	                   for (int j = 0; j < arCustomerCnt[i]; j++) {
+	                      if(arrBank[i][j].phone.equals(phone) && arrBank[i][j].pw.equals(pw)) {
+	                         temp = arrBank[i][j];
+	                      }
+	                   }
+	                }
+	                if(temp == null) {
+	                   System.out.println("조회된 결과가 없습니다.");
+	                   break;
+	                }
+	                System.out.println("조회된 계좌번호 : "+temp.account);
+	
+					
 				default:
 					System.out.println(errors);
 				}
 			}
-
 		}//end while
-
 	}
 }
