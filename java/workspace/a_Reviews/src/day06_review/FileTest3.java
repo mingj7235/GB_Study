830b34cad2da46456b30d032e031cbffb4d38691
mingJ7235 <joshuara7235@gmail.com>
2021-01-28 01:15:47 +0900

    Review File test

--- java/workspace/a_Reviews/src/day06_review/FileTest3.java
+++ java/workspace/a_Reviews/src/day06_review/FileTest3.java
@@ -1,14 +1,63 @@
 package day06_review;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileTest3 {
 	public static void main(String[] args) throws IOException{
-		BufferedWriter bw = new BufferedWriter(new FileWriter("feeling.txt"));
+//		BufferedWriter bw = new BufferedWriter(new FileWriter("feeling.txt"));
+//		
+//		bw.write("기쁨\n");
+//		bw.write("사랑\n");
+//		bw.write("행복\n");
+//		bw.write("감사\n");
+//		bw.close();
+//		
+////		이어쓰기
+//		
+//		BufferedWriter rbw = new BufferedWriter(new FileWriter("feeling.txt", true));
+//		rbw.write("찬양\n");
+//		rbw.close();
 		
-		bw.write("기쁨\n");
+		//수정
+		
+		BufferedReader br = null;
+		String temp = "";
+		
+		try {
+			br = new BufferedReader(new FileReader("feeling.txt"));
+			String line = null;
+
+			while ( (line = br.readLine()) != null ) {
+				if(line.equals("사랑")) {
+					temp += "슬픔";
+					continue;
+				}
+				temp += line + "\n";
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if ( br != null) {
+					br.close();
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("feeling.txt"));
+		bw.write(temp);
+		bw.close();
+		
+		
+		
 		
 		
 	}
