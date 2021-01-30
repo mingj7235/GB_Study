1fe37220ac087caf94df4d3e7265af18d22809dd
mingJ7235 <joshuara7235@gmail.com>
2021-01-30 15:14:22 +0900

    Review IO

--- java/workspace/a_Reviews_boxOffice/src/dao/BoxOfficeDao.java
+++ java/workspace/a_Reviews_boxOffice/src/dao/BoxOfficeDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -12,17 +13,14 @@ public class BoxOfficeDao {
 	
 	//영화 추가
 	private boolean append (BoxOfficeVo film) throws IOException{
-
 		if (film == null) {return false;}
-		
 		BufferedReader br = DBConnection.getReader();
-		if(br == null) {return false;}
-
+		if (br == null) {return false;}
 		String line = null;
 		int lastRanking = 0;
-		
-		while ((line = br.readLine()) != null) {
+		while ( (line = br.readLine()) != null ) {
 			lastRanking ++;
+			//마지막 랭킹이 담기게된다.
 		}
 		br.close();
 		
@@ -30,55 +28,28 @@ public class BoxOfficeDao {
 		
 		String temp = "";
 		
-		if(contents.charAt(contents.length() -1) != '\n') {
-			temp += "\n";
+		if(contents.charAt(contents.length() - 1) != '\n') {
+			temp += 'n';
 		}
 		
-		temp += lastRanking +1 + "\t"
-				+ film.getFilmName() + "\t"
+		temp += lastRanking + 1 + "\t"
+				+film.getFilmName()+ "\t"
 				+ film.getReleaseDate() +"\t"
 				+ film.getIncome() + "\t"
 				+ film.getWatchCnt() + "\t"
 				+ film.getScreenCnt() + "\n";
 		
-		BufferedWriter bw = new DBConnection().getAppend();
+		BufferedWriter bw = DBConnection.getAppend();
 		bw.write(temp);
 		bw.close();
 		
-		return true;
+		return false;
+		
 	}
 	
 	private boolean insert (BoxOfficeVo film) throws IOException {
 		
-		if (film == null) {return false;}
 		
-		int newRanking = film.getRanking();
-		
-		BufferedReader br = DBConnection.getReader();
-		if(br == null) {return false;}
-
-		String line = null;
-		String temp = "";
-		boolean check = false;
-		
-		while ((line = br.readLine()) != null) {
-			if(Integer.parseInt(line.split("\t")[0]) == newRanking) {
-				temp += film.getRanking() +"\t"
-						+ film.getFilmName() + "\t"
-						+ film.getReleaseDate() +"\t"
-						+ film.getIncome() + "\t"
-						+ film.getWatchCnt() + "\t"
-						+ film.getScreenCnt() + "\n";
-				check = true;
-			}
-			
-			
-		}
-		
-		
-		
-		
-		return true;
 	}
 	
 	
@@ -89,3 +60,4 @@ public class BoxOfficeDao {
 	
 	
 }
+ 
