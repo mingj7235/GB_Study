8b78f18418b738bb6ee9724ef56c3a35d5a38ee7
mingJ7235 <joshuara7235@gmail.com>
2021-02-13 13:25:39 +0900

    Naver mail list crawling contitnue (bug fixing 😎)

--- java/workspace/crawling/src/crawling/NaverCrawling.java
+++ java/workspace/crawling/src/crawling/NaverCrawling.java
@@ -49,6 +49,7 @@ public class NaverCrawling {
 		//네이버 가져와서, 로그인 (로그인 입력하도록 하고), 메일 누르고, 메일 내용 출력할수있도록 크롤링
 		
 		driver.findElement(By.className("link_login")).click();
+		threadSleep();
 		
 		System.out.println("아이디를 입력하세요 : ");
 		String id = sc.next();
@@ -61,18 +62,33 @@ public class NaverCrawling {
 		element = driver.findElement(By.id("pw"));
 		element.sendKeys(pw);
 		element.sendKeys(Keys.RETURN);
-		
+		threadSleep();
 		//배열로 가져오기 (메일를 누르는 버튼 자체가 배열로 접근해서 가져와야함
 		
-		List<WebElement> loginbox = driver.findElements(By.className("new_box"));
+		List<WebElement> loginbox = driver.findElements(By.className("link_new"));
+		System.out.println(loginbox.size());
 		loginbox.get(0).click();
+		threadSleep();
+//		element = driver.findElement(By.id("sender_context")); // 네이버 메일 리스트 정보
+//		
+//		element.findElements(By.className(""))
 		
-		element = driver.findElement(By.id("list_for_view")); // 네이버 메일 리스트 정보
+		List<WebElement> Senders = driver.findElements(By.className("_ccr(lst.from)"));
+		List<WebElement> Subjects = driver.findElements(By.className("subject"));
+		
+		for (int i = 0; i < Senders.size(); i++) {
+			String sender = Senders.get(i).findElement(By.className("_stopDefault")).getText();
+			String subject = Subjects.get(i).findElement(By.className("mail_title")).getText();
+			
+			System.out.println(id + " 님의 메일리스트"
+					+ "\n보낸 사람 : " + sender + " 메일 제목 : " + subject );
+			
+		}		
 		
 	}
 	
 	public void threadSleep () {
-		try {Thread.sleep(1000);} catch (InterruptedException e) {}
+		try {Thread.sleep(500);} catch (InterruptedException e) {}
 	}
 	
 	
