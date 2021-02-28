cc12912c72e55cc9e4c1e1b9f2058738eaaadd8b
mingJ7235 <joshuara7235@gmail.com>
2021-02-28 23:10:15 +0900

    Signup method completed 💪

--- jsp/workspace/myFrontProject/WebContent/member.js
+++ jsp/workspace/myFrontProject/WebContent/member.js
@@ -5,7 +5,7 @@
 var check = false;
 
 function modifyId () {
-	document.signupForm.email.readOnly = false;
+	document.signupForm.id.readOnly = false;
 	check = false;
 }
 
@@ -13,19 +13,19 @@ function checkId() {
 	//Ajax로 중복 체크 해주기.
 	
 	var httpRequest = new XMLHttpRequest();
-	var email = document.getElementById(email).value;
-	httpRequest.open("GET", "signup_check.jsp?email="+email);
+	var id = document.getElementById("id").value;
+	httpRequest.open("GET", "signup_check.jsp?id="+id);
 	httpRequest.send();
 	
 	httpRequest.onreadystatechange = function () {
 		if(httpRequest.readyState == XMLHttpRequest.DONE && 
 				httpRequest.status ==200) {
 			if(httpRequest.responseText.trim() == "ok") {
-				document.signupForm.email.readOnly = true;
-				document.getElementById("check_id_result").innerHTML ="사용가능한 아이디입니다.";
+				document.signupForm.id.readOnly = true;
+				document.getElementById("check_id_result").innerHTML ="<span style ='color:red;'>*</span>사용가능한 아이디입니다.";
 				check = true;
 			} else {
-				document.getElementById("check_id_result").innerHTML ="중복된 아이디입니다.";
+				document.getElementById("check_id_result").innerHTML ="<span style ='color:red;'>*</span>중복된 아이디입니다.";
 				check =  true;
 			}
 		}
@@ -33,6 +33,12 @@ function checkId() {
 }
 
 function signup() {
+	var frm = document.signupForm;
 	
+	if(frm.id.value == "") {
+		alert("아이디를 입력해주세요");
+		frm.id.focus();
+		return false;
+	}
 	
 }
