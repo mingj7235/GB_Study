5d687da1ad6f74c5cbd75d622557ffc696d9eca6
mingJ7235 <joshuara7235@gmail.com>
2021-02-28 22:45:53 +0900

    Front checkId method done !

---
+++ jsp/workspace/myFrontProject/WebContent/member.js
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+var check = false;
+
+function checkId() {
+	//Ajax로 중복 체크 해주기.
+	
+	var httpRequest = new XMLHttpRequest();
+	var email = document.getElementById(email).value;
+	httpRequest.open("GET", "signup_check.jsp?email="+email);
+	httpRequest.send();
+	
+	httpRequest.onreadystatechange = function () {
+		if(httpRequest.readyState == XMLHttpRequest.DONE && 
+				httpRequest.status ==200) {
+			if(httpRequest.responseText.trim() == "ok") {
+				document.signupForm.id.readOnly = true;
+				document.getElementById("check_id_result").innerHTML ="사용가능한 아이디입니다.";
+				check = true;
+			} else {
+				document.getElementById("check_id_result").innerHTML ="중복된 아이디입니다.";
+				check =  true;
+			}
+		}
+	}
+}
