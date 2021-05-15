b4a0f7a9aee609e0d971791aeea5680bd77872d4
mingJ7235 <joshuara7235@gmail.com>
2021-05-15 15:53:16 +0900

    Autowired required option (-> false : xml objects ignore)

--- spring/workspace/springNewLecture/src/springNewLecture/di/ui/InlineExamConsole.java
+++ spring/workspace/springNewLecture/src/springNewLecture/di/ui/InlineExamConsole.java
@@ -7,21 +7,42 @@ import springNewLecture.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
 	
+	@Autowired (required = false)
+		//required를 false를 주면 xml에서 그 찾는 객체가 없는경우에도
+		//실행이된다. 즉, 옵션을 주고싶을때, 분기를 줘서 처리가능하다.
+		//원래는 이 옵션을 주지않으면 오류가 난다. 
+	@Qualifier("exam1")
+	//기본생성자를 통해서 인젝션이 되는것이므로
+	//기본생성자가 없으면 인젝션이 되지않는다.
+	
 	private Exam exam;
 	
 	public InlineExamConsole() {
+		System.out.println("constructor");
 	}
 	
-	public InlineExamConsole(Exam exam) {
+	//오버로딩된 생성자에 autowired를 사용할경우
+	//매개변수에서 지정해야한다.
+	//파라미터에서 몇가지의 파라미터가 있을 수 있기때문이다.
+
+	//@Autowired
+	public InlineExamConsole(/*@Qualifier("exam1")*/ Exam exam) {
+		System.out.println("overloaded constructor");
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if(exam == null) {
+			System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		}else {
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		}
 	}
 	
-	@Autowired //자동으로 객체를 연결해달라! setting.xml 의 설정을 가져와준다.
+	
+	
+	//@Autowired //자동으로 객체를 연결해달라! setting.xml 의 설정을 가져와준다.
 	//근데 autowired는 대체 어떤 근거로 그 객체를 참조하여 세팅해주는가?
 	
 	/*autowired는 컨테이너에서 참조되는 객체를 즉, 인터페이스 (class)를
@@ -33,7 +54,7 @@ public class InlineExamConsole implements ExamConsole {
 	 * 그럴때 나온게 바로 @Qualifier다.
 	 * 
 	 * */
-	@Qualifier ("exam1")
+	//@Qualifier ("exam1")
 	/*
 	 * 이곳에 알맞은 xml bean의 아이디를 지정해주면 올바르게 바인딩이된다.
 	 * Autowired는 무엇을 기준으로하는지, 그리고 Qualifier를 왜쓰는지
@@ -43,6 +64,7 @@ public class InlineExamConsole implements ExamConsole {
 	 * */
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter");
 		this.exam = exam;
 	}
 	
