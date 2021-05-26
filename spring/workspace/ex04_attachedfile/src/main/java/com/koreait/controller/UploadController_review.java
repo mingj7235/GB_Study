a60b99fb6bdb62549c939b77ca151a33da787131
김민재 <joshuara7235@gmail.com>
2021-05-26 20:37:35 +0900

    Review for ajax way upload files for Spring project

--- spring/workspace/ex04_attachedfile/src/main/java/com/koreait/controller/UploadController_review.java
+++ spring/workspace/ex04_attachedfile/src/main/java/com/koreait/controller/UploadController_review.java
@@ -2,6 +2,9 @@ package com.koreait.controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -50,11 +53,61 @@ public class UploadController_review {
 	}
 	
 	@PostMapping ("/uploadAjaxActionReview")
-	public void uploadAjaxActionReview () {
+	public void uploadAjaxActionReview (MultipartFile[] uploadFiles) {
+		log.info("upload ajax post....");
+		
+		File uploadPath = new File(uploadFolder, getFolder());
+		
+		if (!uploadPath.exists()) {
+			uploadPath.mkdir();
+		}
+				
+		
+		for (MultipartFile multipartFile : uploadFiles) {
+			
+			String fileName = multipartFile.getOriginalFilename();
+			Long fileSize = multipartFile.getSize();
+			UUID uuid = UUID.randomUUID();
+			
+			fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
+			fileName = uuid.toString() + "_" + fileName;
+			
+			File saveFile = new File(uploadFolder, fileName);
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 		
 	}
 	
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
